91285c2678e82a74b33640a63857f53a803b4d41
Pietro Martinelli <amicofragile@gmail.com>
2013-05-23 22:33:05 +0200

    REFACTORING Extracted constants for competitor names

--- src/test/java/org/amicofragile/cninja/MatchTest.java
+++ src/test/java/org/amicofragile/cninja/MatchTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MatchTest {
+	private static final String GUEST = "B";
+	private static final String HOME = "A";
+
 	@Test
 	public void testConstructorAndGetters() throws Exception {
-		Match match = new Match("A", "B");
-		assertEquals("A", match.getHome());
-		assertEquals("B", match.getGuest());
+		Match match = new Match(HOME, GUEST);
+		assertEquals(HOME, match.getHome());
+		assertEquals(GUEST, match.getGuest());
 	}
 }
